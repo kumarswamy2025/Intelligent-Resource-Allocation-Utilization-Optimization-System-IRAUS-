ecc13480f6dc890a9d3eb5e3a6c53281dc50d930
kumar swamy <yarramneedikumarswamy@gmail.com>
2026-02-27 22:50:47 +0530

    spring boot project is setuped successfully

---
+++ Backend/IRAUS/src/main/java/com/iraus/iraus/IrausApplication.java
@@ -0,0 +1,13 @@
+package com.iraus.iraus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IrausApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IrausApplication.class, args);
+    }
+
+}
---
+++ Backend/IRAUS/src/test/java/com/iraus/iraus/IrausApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iraus.iraus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IrausApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
