fcee2145a65cf1e914750a68d6f4d8b05337999e
kumar swamy <yarramneedikumarswamy@gmail.com>
2026-02-27 23:06:17 +0530

    testing controller for demo

---
+++ Backend/IRAUS/src/main/java/com/iraus/iraus/controller/AuthenticationController/AuthController.java
@@ -0,0 +1,15 @@
+package com.iraus.iraus.controller.AuthenticationController;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/auth")
+public class AuthController {
+
+    @GetMapping("/login")
+    public String Test(){
+        return "test";
+    }
+}
