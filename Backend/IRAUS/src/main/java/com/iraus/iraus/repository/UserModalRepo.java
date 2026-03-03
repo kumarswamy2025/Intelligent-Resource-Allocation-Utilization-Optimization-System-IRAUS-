7a559147b73ed03704be08430aefa02313137eca
kumar swamy <yarramneedikumarswamy@gmail.com>
2026-03-03 11:45:55 +0530

    creating repository class

--- Backend/IRAUS/src/main/java/com/iraus/iraus/repository/UserModalRepo.java
+++ Backend/IRAUS/src/main/java/com/iraus/iraus/repository/UserModalRepo.java
@@ -6,7 +6,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface UserRepo extends JpaRepository<UserModal,Integer> {
+public interface UserModalRepo extends JpaRepository<UserModal,Integer> {
 
-    UserModal findByEmail(String email);
+
 }
