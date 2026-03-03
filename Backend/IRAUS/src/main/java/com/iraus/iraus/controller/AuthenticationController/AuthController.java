765b02cf25c7f3dc8082a04de561759a736414e0
kumar swamy <yarramneedikumarswamy@gmail.com>
2026-03-03 12:27:26 +0530

    updating auth controller

--- Backend/IRAUS/src/main/java/com/iraus/iraus/controller/AuthenticationController/AuthController.java
+++ Backend/IRAUS/src/main/java/com/iraus/iraus/controller/AuthenticationController/AuthController.java
@@ -1,11 +1,6 @@
 package com.iraus.iraus.controller.AuthenticationController;
 
-import com.iraus.iraus.Service.Services.AuthService;
-import com.iraus.iraus.exceptions.UserException.UserExceptions;
-import com.iraus.iraus.payload.DTO.UserDTO;
-import com.iraus.iraus.payload.response.AuthResponse;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
