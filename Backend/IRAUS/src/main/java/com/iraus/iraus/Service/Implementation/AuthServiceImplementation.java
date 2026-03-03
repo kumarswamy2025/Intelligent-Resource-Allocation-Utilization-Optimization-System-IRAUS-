e8cbfc12e70e2336dbb2af0d4d73c3ae45662ed4
kumar swamy <yarramneedikumarswamy@gmail.com>
2026-03-03 12:29:45 +0530

    working on auth service implementatioon'

---
+++ Backend/IRAUS/src/main/java/com/iraus/iraus/Service/Implementation/AuthServiceImplementation.java
@@ -0,0 +1,18 @@
+package com.iraus.iraus.Service.Implementation;
+
+import com.iraus.iraus.Service.Services.AuthService;
+import com.iraus.iraus.exceptions.UserException.UserExceptions;
+import com.iraus.iraus.payload.DTO.UserDTO;
+import com.iraus.iraus.payload.response.AuthResponse;
+
+public class AuthServiceImplementation implements AuthService {
+    @Override
+    public AuthResponse signup(UserDTO userDTO) throws UserExceptions {
+        return null;
+    }
+
+    @Override
+    public AuthResponse login(UserDTO userDTO) throws UserExceptions {
+        return null;
+    }
+}
